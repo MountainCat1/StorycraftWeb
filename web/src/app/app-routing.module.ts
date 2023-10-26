0427fa8cdca198ea2b8aaf596666ad2e46d9da1f
Mountain Cat <tomasz.banas123@gmail.com>
2023-10-27 01:04:17 +0200

    work work

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import {ExtraOptions, RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {InfoComponent} from "./info/info.component";
+import {ContactComponent} from "./contact/contact.component";
 
 
 const APP_ROUTES: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect from root to 'home'
   { path: 'home', component: HomeComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'sing-up', component: ContactComponent },
 ];
 
 export const routingConfiguration: ExtraOptions = {
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -10,11 +10,15 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import { HomeComponent } from './home/home.component';
 import {NgOptimizedImage} from "@angular/common";
+import { InfoComponent } from './info/info.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    InfoComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
